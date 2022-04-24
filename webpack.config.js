9445f2e990c178ccf1d4d2c34a0211d6ceb145e7
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-04-24 13:56:18 -0500

    fixed package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,24 @@
-const path = require('path');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require('path');
 
-// main configuration; we write options within this object to instruct webpack
-module.exports = {
-  // entry is where the webpack looks to start building the module
+const config = {
   entry: {
     app: './assets/js/script.js',
     events: './assets/js/events.js',
     schedule: './assets/js/schedule.js',
     tickets: './assets/js/tickets.js',
   },
-  // used to tell webpack where the files will go and what their names are.
   output: {
     filename: '[name].bundle.js',
-    path: __dirname + '/dist',
+    path: `${__dirname}/dist`,
   },
   module: {
     rules: [
-      // this object will identify the type of files to pre-process using the test property
       {
-        test: /\.jpg$/,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
@@ -30,7 +27,7 @@ module.exports = {
               name(file) {
                 return '[path][name].[ext]';
               },
-              publicPath: function (url) {
+              publicPath(url) {
                 return url.replace('../', '/assets/');
               },
             },
@@ -48,11 +45,24 @@ module.exports = {
       jQuery: 'jquery',
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static',
     }),
+    // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
   ],
-  // development or production are options.
-  // development will offer hot reloading of webpack and debugging features.
-  // production will run uglify and build your source files into minimize files.
   mode: 'development',
 };
+
+module.exports = config;
